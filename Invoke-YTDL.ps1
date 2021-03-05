975130b5dd4ac867be187ade0453b397e99b2b7f
BusyBread <47672746+BusyBread@users.noreply.github.com>
2021-03-05 15:03:43 -0600

    Fixed typos

--- Invoke-YTDL.ps1
+++ Invoke-YTDL.ps1
@@ -7,7 +7,7 @@
     version from the developers site. See Get-YTDLEXE function for more. Default path is %localappdata%\youtube-dl
 
     .Parameter DownloadPath
-    Path to save downloaded files, if not already present the script will attempt to create the directory. Defalts folder to users Downloads.
+    Path to save downloaded files, if not already present the script will attempt to create the directory. Defaults folder to users Downloads.
 
     .Parameter YT_Module
     Path to ytdl.psm1
@@ -16,7 +16,7 @@
     Created 03/05/2021 by EAM
 #>
 param(
-    $YTDL_Executable = (Join-Path -Path $env:LOCALAPPDATA -ChildPath "someotherpath\youtube-dl.exe"),
+    $YTDL_Executable = (Join-Path -Path $env:LOCALAPPDATA -ChildPath "youtube-dl\youtube-dl.exe"),
     $DownloadPath = (Join-Path -Path $env:USERPROFILE -ChildPath "Downloads\yt_downloads"),
     $YT_Module = (Join-Path -Path $PWD -ChildPath "src\ytdl.psm1")
 )
